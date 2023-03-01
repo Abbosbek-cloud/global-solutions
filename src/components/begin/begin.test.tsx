979a7a66df6df2bdc8e173375ff23459035f61ae
Abbosbek-cloud <abek01sulaymonov@gmail.com>
2023-03-02 02:51:24 +0500

    describe function to grab tests

--- src/components/begin/begin.test.tsx
+++ src/components/begin/begin.test.tsx
@@ -1,14 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import Begin from "./begin";
 
-test("Test begin component", () => {
-  render(<Begin />);
-  const textElement = screen.getByText(/Begin/i);
-  expect(textElement).toBeInTheDocument();
-}, 3000);
+describe("Begin", () => {
+  test("Test begin component", () => {
+    render(<Begin />);
+    const textElement = screen.getByText(/Begin/i);
+    expect(textElement).toBeInTheDocument();
+  }, 3000);
 
-test("Begin test with props", () => {
-  render(<Begin name="lorem" />);
-  const textElement = screen.getByText(/Begin lorem/i);
-  expect(textElement).toBeInTheDocument();
+  test("Begin test with props", () => {
+    render(<Begin name="lorem" />);
+    const textElement = screen.getByText(/Begin lorem/i);
+    expect(textElement).toBeInTheDocument();
+  });
 });

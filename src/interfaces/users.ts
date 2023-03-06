214a2859cccb81165ae8a0f94425be5d0aeef10c
Abbosbek-cloud <abek01sulaymonov@gmail.com>
2023-03-07 02:07:14 +0500

    edited

--- src/interfaces/users.ts
+++ src/interfaces/users.ts
@@ -1,6 +1,14 @@
+import { Product } from "./products";
+
 export interface User {
   name?: string;
   age?: number;
   friends?: string[];
   status?: string;
+}
+
+export interface UserOrders {
+  id: string;
+  products: Product[];
+  address: string;
 }
